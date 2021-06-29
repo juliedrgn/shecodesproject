e84223649f9ed101a988d9721b6509886eb0d0d9
juliedrgn <86077434+juliedrgn@users.noreply.github.com>
2021-06-29 20:10:20 +0200

    updated icon

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
-function formatTime(date) {
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -7,7 +8,6 @@ function formatTime(date) {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  let dayIndex = date.getDay();
   let days = [
     "Sunday",
     "Monday",
@@ -17,12 +17,9 @@ function formatTime(date) {
     "Friday",
     "Saturday",
   ];
-  let day = days[dayIndex];
+  let day = days[date.getDay()];
   return `${day} | ${hours}: ${minutes}`;
 }
-let datum = document.querySelector("#current-date");
-let currentTime = new Date();
-datum.innerHTML = formatTime(currentTime);
 
 function displayTemp(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -30,15 +27,23 @@ function displayTemp(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windspeedElement = document.querySelector("#windspeed");
+  let dateElement = document.querySelector("#current-date");
+  let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windspeedElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 let apiKey = "a921daf97c39af523ba6c55cc2fd35f9";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}`;
+let city = "Paris";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
 axios.get(apiUrl).then(displayTemp);
