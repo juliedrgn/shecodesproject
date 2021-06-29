4b902f5fba744f8a511bfebd3927a1550c0a2a30
juliedrgn <86077434+juliedrgn@users.noreply.github.com>
2021-06-29 18:33:43 +0200

    js update temperature - weather description

--- src/script.js
+++ src/script.js
@@ -24,41 +24,21 @@ let datum = document.querySelector("#current-date");
 let currentTime = new Date();
 datum.innerHTML = formatTime(currentTime);
 
-
-function showTemperature(response) {
-  document.querySelector("#current-city").innerHTML = response.data.name;
-  let temp = Math.round(response.data.main.temp);
-  let position = document.querySelector("#temperature");
-  position.innerHTML = `${temp}°`;
-}
-
-function search(event) {
-  event.preventDefault();
-  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
-  let city = document.querySelector("#search-input").value;
-  let apiKey = "a921daf97c39af523ba6c55cc2fd35f9";
-  let units = "metric";
-  let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
+function displayTemp(response) {
+  let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#current-city");
-  let cityInput = document.querySelector("#search-input");
-  cityElement.innerHTML = cityInput.value;
-  console.log(apiUrl);
-  axios.get(apiUrl).then(showTemperature);
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windspeedElement = document.querySelector("#windspeed");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windspeedElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
+let apiKey = "a921daf97c39af523ba6c55cc2fd35f9";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}`;
 
-function showCurrentPosition(position) {
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let units = "metric";
-  let apiKey = "a921daf97c39af523ba6c55cc2fd35f9";
-  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
-  let apiUrl = `${apiEndpoint}?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(showTemperature);
-}
-navigator.geolocation.getCurrentPosition(showCurrentPosition);
-
-let currentLocationBtn = document.querySelector("#button-current-location");
-currentLocationBtn.addEventListener("click", showCurrentPosition);
+axios.get(apiUrl).then(displayTemp);
