8df39573d21fac772f15cd1631cda8b6d85a0144
juliedrgn <86077434+juliedrgn@users.noreply.github.com>
2021-06-30 21:07:40 +0200

    postion button

--- src/script.js
+++ src/script.js
@@ -42,8 +42,43 @@ function displayTemp(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+function displayWeather(response) {
+  document.querySelector("#search-input").innerHTML = response.data.name;
+}
+function search(event) {
+  event.preventDefault();
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiKey = "a921daf97c39af523ba6c55cc2fd35f9";
+  let city = document.querySelector("#search-input").value;
+  let units = "metric";
+  let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
+  let cityElement = document.querySelector("#current-city");
+  let cityInput = document.querySelector("#search-input");
+  cityElement.innerHTML = cityInput.value;
+  axios.get(apiUrl).then(displayTemp);
+}
+
+function searchPosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let units = "metric";
+  let apiKey = "a921daf97c39af523ba6c55cc2fd35f9";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${apiEndpoint}?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayTemp);
+}
+function getCurrentPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchPosition);
+}
 
 let apiKey = "a921daf97c39af523ba6c55cc2fd35f9";
 let city = document.querySelector("#search-input").value;
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 axios.get(apiUrl).then(displayTemp);
+
+let form = document.querySelector("#search-form");
+form.addEventListener("click", search);
+
+let currentLocationBtn = document.querySelector("#button-current-location");
+currentLocationBtn.addEventListener("click", searchPosition);
